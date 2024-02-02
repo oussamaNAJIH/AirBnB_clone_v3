6a79ffa92b44f1a6957fbb19d107ccb3c31caaa5
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 18:35:14 +0100

    State

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-from flask import Flask, render_template, jsonify
+"""
+Module for creating and configuring the Flask application
+"""
+
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
+
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
-def teardown_flask(exception):
-    '''calls storage.close()'''
+def teardown_appcontext(exception):
+    """Closes the storage on teardown"""
     storage.close()
 
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return jsonify(error='Not found'), 404
-
-
-if __name__ == '__main__':
-    if getenv("HBNB_API_HOST"):
-        host = getenv("HBNB_API_HOST")
-    else:
-        host = '0.0.0.0'
-    if getenv("HBNB_API_PORT"):
-        port = int(getenv("HBNB_API_PORT"))
-    else:
-        port = 5000
-    app.run(host, port, threaded=True)
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-python file
+Package initializer for views
 """
+
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+# Create a variable app_views which is an instance of Blueprint
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Wildcard import of everything in the package api.v1.views.index
 from api.v1.views.index import *
 from api.v1.views.states import *

33f669eebce9f6c16ac9fa3907efeaa9b3ec32fa
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 15:00:51 +0100

    files

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,7 +17,7 @@ def teardown_flask(exception):
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return jsonify({"error": "Not Found"}), 404
+    return jsonify(error='Not found'), 404
 
 
 if __name__ == '__main__':

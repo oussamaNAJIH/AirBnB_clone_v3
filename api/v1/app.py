6d51c18ac986fc9ae884ee5c386cf21d879c53f0
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 22:56:07 +0100

    file

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-from models import *
 
 app = Flask(__name__)
 app.register_blueprint(app_views)

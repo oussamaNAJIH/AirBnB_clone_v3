e27aec35f9e73746aa566c0361e7c9f0cf1703c0
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 14:44:40 +0100

    files

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,50 +1,32 @@
 #!/usr/bin/python3
-'''Contains a Flask web application API.
-'''
-import os
-from flask import Flask, jsonify
-from flask_cors import CORS
-
+"""script that starts a Flask web application"""
+from flask import Flask, render_template, jsonify
 from models import storage
 from api.v1.views import app_views
-
+from os import getenv
 
 app = Flask(__name__)
-'''The Flask web application instance.'''
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
 
 
 @app.teardown_appcontext
 def teardown_flask(exception):
-    '''The Flask app/request context end event listener.'''
-    # print(exception)
+    '''calls storage.close()'''
     storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    '''Handles the 404 HTTP error code.'''
-    return jsonify(error='Not found'), 404
-
-
-@app.errorhandler(400)
-def error_400(error):
-    '''Handles the 400 HTTP error code.'''
-    msg = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        msg = error.description
-    return jsonify(error=msg), 400
+def not_found_error(error):
+    return jsonify({"error": "Not Found"}), 404
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    if getenv("HBNB_API_HOST"):
+        host = getenv("HBNB_API_HOST")
+    else:
+        host = '0.0.0.0'
+    if getenv("HBNB_API_PORT"):
+        port = int(getenv("HBNB_API_PORT"))
+    else:
+        port = 5000
+    app.run(host, port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-python file
-"""
+'''Contains the blueprint for the API.'''
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
+
 
 from api.v1.views.index import *
 from api.v1.views.states import *

cb3716d02ecbb0964a4c07554ddc863570b15377
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 20:32:38 +0100

    amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -12,7 +12,7 @@ from models.amenity import Amenity
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
 def get_amenities():
     """Retrieves the list of all Amenity objects"""
-    amenities = [state.to_dict() for state in storage.all(Amenity).values()]
+    amenities = [amenity.to_dict() for amenity in storage.all(Amenity).values()]
     return jsonify(amenities)
 
 
@@ -28,17 +28,17 @@ def get_amenity(amenity_id):
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id):
-    """Deletes a State object"""
+    """Deletes an Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
     storage.delete(amenity)
     storage.save()
-    return jsonify({})
+    return jsonify({}), 200
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def create_state():
+def create_amenity():
     """Creates a Amenity object"""
     data = request.get_json()
     if data is None:
@@ -46,11 +46,11 @@ def create_state():
     if 'name' not in data:
         return jsonify({"error": "Missing name"}), 400
     new_amenity = Amenity(**data)
-    new_amenity.save()
+    storage.save()
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
 def update_amenity(amenity_id):
     """Updates a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)

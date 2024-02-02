1d6965e612a51e0c47dc2a97e1a66b9f6561124c
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 14:45:13 +0100

    files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-'''Contains the blueprint for the API.'''
+"""
+python file
+"""
 from flask import Blueprint
 
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-'''The blueprint for the AirBnB clone API.'''
-
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *

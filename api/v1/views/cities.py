2943749d4845dfdcef5dad5f4f72b2c0f504defd
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-02 20:16:48 +0100

    City

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -42,7 +42,8 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_city(state_id):
     """Creates a State object"""
     state = storage.get(State, state_id)

87e8f21fa0e09ed0ae284df75fe8298a79eadf5a
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-03 10:16:28 +0100

    user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,3 +2,14 @@
 """
 Module for user views
 """
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'])
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = [user.to_dict() for user in storage.all(User).values()]
+    return jsonify(users)

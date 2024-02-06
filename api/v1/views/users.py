0e020c330075194c28be35936fc1e1e7727f0fcf
oussamaNAJIH <oussamanajih6@gmail.com>
2024-02-06 17:13:32 +0100

    users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -2,7 +2,7 @@
 """
 Module for user views
 """
-from flask import Flask, jsonify, abort, request
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.user import User
@@ -15,8 +15,7 @@ def get_users():
     return jsonify(users)
 
 
-@app_views.route('/users/<user_id>',
-                 methods=['GET'])
+@app_views.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
     """Retrieves a User object"""
     user = storage.get(User, user_id)
@@ -27,7 +26,7 @@ def get_user(user_id):
 
 @app_views.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    """Deletes an User object"""
+    """Deletes a User object"""
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
@@ -38,7 +37,7 @@ def delete_user(user_id):
 
 @app_views.route('/users', methods=['POST'])
 def create_user():
-    """Creates an User object"""
+    """Creates a User object"""
     data = request.get_json()
     if data is None:
         return jsonify({"error": "Not a JSON"}), 400
@@ -47,25 +46,22 @@ def create_user():
     if 'password' not in data:
         return jsonify({"error": "Missing password"}), 400
     new_user = User(**data)
+    storage.new(new_user)
     storage.save()
     return jsonify(new_user.to_dict()), 201
 
 
-@app_views.route('/users/<user_id>',
-                 methods=['PUT'])
+@app_views.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
-    """Updates an User object"""
+    """Updates a User object"""
     user = storage.get(User, user_id)
     if user is None:
         abort(404)
-
     data = request.get_json()
     if data is None:
         return jsonify({"error": "Not a JSON"}), 400
-
     for key, value in data.items():
         if key not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(user, key, value)
-
     storage.save()
-    return jsonify(user.to_dict())
+    return jsonify(user.to_dict()), 200
